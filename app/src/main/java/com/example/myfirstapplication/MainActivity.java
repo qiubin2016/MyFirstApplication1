fa49a892f9de4de087cc4b2056d07918bba905b0
crazy <qiubin2018@gmail.com>
2018-06-09 15:29:08 +0800

    第一个activity里点击按钮能跳转到第二个activity里。

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.myfirstapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**Called when the user taps the Send button
+     * 要让系统将此函数视为与 android:onClick 属性兼容，需要这些详细信息。具体来说，函数必须声明以下内容：
+     1.公共访问
+     2.空返回值
+     3.以 View 作为唯一参数（它是之前点击的 View 对象）*/
+    public void sendMessage(View view){
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
